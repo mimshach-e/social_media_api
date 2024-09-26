96068d458f44e9b888fcf62a9d3e77058b0441d7
Ernest Mimshach <mimshach.e@gmail.com>
2024-09-26 16:52:12 +0000

    made changes to projects urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('posts.urls')),
     path('api/', include('notifications.urls')),
+    path('', views.home, name='home'),
 
 ]

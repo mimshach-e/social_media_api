82b5a44d9efc20cf940f4ecf5c220b7a96413c7e
Ernest Mimshach <mimshach.e@gmail.com>
2024-09-26 17:48:54 +0000

    made changes to files

--- social_media_api/social_media_api/views.py
+++ social_media_api/social_media_api/views.py
@@ -1,4 +1,4 @@
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
-# def home(request):
-#     return HttpResponse("Welcome to the Social Media API!")
+def home(request):
+    return HttpResponse("Welcome to the Social Media App API!")
